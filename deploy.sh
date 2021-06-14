2d56146517cb1baa61a63cb22d31e3f05c12d5f2
Apollo314 <abidinsefa@outlook.com>
2021-06-15 02:17:23 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Apollo314/cassette-player.git main:gh-pages
+git push -f git@github.com:Apollo314/cassette-player.git master:gh-pages
