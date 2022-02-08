60e839e82b2c832f779b1a208ae0750b5bbeceac
Apollo314 <abidinsefa@outlook.com>
2022-02-08 04:16:59 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/cassette-player/'
-      : '/'
+      : '/',
+    devServer: {
+      compress: true,
+      disableHostCheck: true,   // That solved it
+    }
   }
   
